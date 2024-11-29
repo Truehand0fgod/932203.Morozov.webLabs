1a0751364e2354d7277ba84acb6bcd6fba4a4cae
Truehand0fgod <galilei923@gmail.com>
2024-11-29 18:03:09 +0700

    lab11_done

---
+++ lab11/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using lab11.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace lab11.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UsingModel()
+        {
+            NumbersOperations data = GetData();
+            return View(data);
+        }
+
+        public IActionResult UsingViewData()
+        {
+            ViewData["data"] = GetData();
+            return View();
+        }
+
+        public IActionResult UsingViewBag()
+        {
+            ViewBag.data = GetData();
+            return View();
+        }
+
+        public IActionResult UsingServiceInjection()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public NumbersOperations GetData()
+        {
+            Random random = new Random();
+            (var firstNumber, var secondNumber) = (random.Next(11), random.Next(11));
+
+            return new NumbersOperations
+            {
+                firstNumber = firstNumber,
+                secondNumber = secondNumber,
+                sum = firstNumber + secondNumber,
+                sub = firstNumber - secondNumber,
+                mul = firstNumber * secondNumber,
+                div = checkDivisionByZero(firstNumber, secondNumber)
+            };
+        }
+        public int checkDivisionByZero(int firstNumber, int secondNumber)
+        {
+            try
+            {
+                var divResult = firstNumber / secondNumber;
+                return divResult;
+            }
+            catch (DivideByZeroException)
+            {
+                return  -1;
+            }
+        }
+    }
+}
---
+++ lab11/Services/GetDataService.cs
@@ -0,0 +1,42 @@
+﻿namespace lab11.Services
+{
+    public class GetDataService : IGetData
+    {
+        public int firstNumber { get; private set; }
+        public int secondNumber { get; private set; }
+
+        public GetDataService()
+        {
+            Random random = new Random();
+            (firstNumber, secondNumber) = (random.Next(11), random.Next(11));
+        }
+
+        public int Sum()
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public int Mul()
+        {
+            return firstNumber * secondNumber;
+        }
+
+        public int Sub()
+        {
+            return firstNumber - secondNumber;
+        }
+
+        public int Div()
+        {
+            try
+            {
+                var divResult = firstNumber / secondNumber;
+                return divResult;
+            }
+            catch (DivideByZeroException)
+            {
+                return -1;
+            }
+        }
+    }
+}
